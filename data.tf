5fd6a1ac36f41dfd39ace37df9b8b7ccebb2fa7e
Venkatesh <tvenky359@gmail.com>
2025-12-17 23:33:33 +0530

    Changes added

--- data.tf
+++ data.tf
@@ -9,21 +9,21 @@ data "aws_availability_zones" "available" {
 # ============================================
 # Get details of the default VPC in this AWS account & region
 # ============================================
-# data "aws_vpc" "default" {
-#   # Setting this to true fetches the default VPC that AWS creates automatically
-#   default = true
-# }
 data "aws_vpc" "default" {
-  id = "vpc-014d925878bec0fa9"
+  # Setting this to true fetches the default VPC that AWS creates automatically
+  default = true
 }
+# data "aws_vpc" "default" {
+#   id = "vpc-014d925878bec0fa9"
+# }
 
 # ============================================
 # Get details of the main route table of the default VPC
 # ============================================
 data "aws_route_table" "main" {
   # Uses the default VPC ID from the data source above
-  #vpc_id = data.aws_vpc.default.id
-  vpc_id = aws_vpc.main.id
+  vpc_id = data.aws_vpc.default.id
+  # vpc_id = aws_vpc.main.id
 
   # Filter ensures we only get the "main" route table of that VPC
   filter {
